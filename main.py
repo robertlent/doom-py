00704ff8e41a945276826df400a41851682e6ba7
Robert Lent <robert@therobertlent.com>
2023-03-22 18:49:30 -0700

    Add textures, implement mouse control for turning

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pygame as pg
 import sys
 from settings import *
-from map import *
+from game_map import *
 from player import *
 from raycasting import *
+from object_renderer import *
 
 
 class Game:
     def __init__(self):
         pg.init()
+        pg.mouse.set_visible(False)
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
         self.delta_time = 1
@@ -17,6 +19,7 @@ class Game:
     def new_game(self):
         self.map = Map(self)
         self.player = Player(self)
+        self.object_renderer = ObjectRenderer(self)
         self.raycasting = Raycasting(self)
 
     def update(self):
@@ -27,9 +30,10 @@ class Game:
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
     def draw(self):
-        self.screen.fill('black')
-        self.map.draw()
-        self.player.draw()
+        # self.screen.fill('black')
+        self.object_renderer.draw()
+        # self.map.draw()
+        # self.player.draw()
 
     def check_events(self):
         for event in pg.event.get():
--- raycasting.py
+++ raycasting.py
@@ -6,11 +6,39 @@ from settings import *
 class Raycasting:
     def __init__(self, game):
         self.game = game
+        self.ray_casting_result = []
+        self.objects_to_render = []
+        self.textures = self.game.object_renderer.wall_textures
+
+    def get_objects_to_render(self):
+        self.objects_to_render = []
+
+        for ray, values in enumerate(self.ray_casting_result):
+            depth, projection_height, texture, offset = values
+
+            if projection_height < HEIGHT:
+                wall_column = self.textures[texture].subsurface(
+                    offset * (TEXTURE_SIZE - SCALE), 0, SCALE, TEXTURE_SIZE)
+                wall_column = pg.transform.scale(
+                    wall_column, (SCALE, projection_height))
+                wall_pos = (ray * SCALE, HALF_HEIGHT - projection_height // 2)
+            else:
+                texture_height = TEXTURE_SIZE * HEIGHT / projection_height
+                wall_column = self.textures[texture].subsurface(
+                    offset * (TEXTURE_SIZE - SCALE), HALF_TEXTURE_SIZE -
+                    texture_height // 2, SCALE, texture_height
+                )
+                wall_column = pg.transform.scale(wall_column, (SCALE, HEIGHT))
+                wall_pos = (ray * SCALE, 0)
+
+            self.objects_to_render.append((depth, wall_column, wall_pos))
 
     def ray_cast(self):
+        self.ray_casting_result = []
         ox, oy = self.game.player.pos
         x_map, y_map = self.game.player.map_pos
         ray_angle = self.game.player.angle - HALF_FOV + 0.0001
+        texture_vertical, texture_horizontal = 1, 1
 
         for ray in range(NUM_RAYS):
             sin_a = math.sin(ray_angle)
@@ -28,6 +56,7 @@ class Raycasting:
                 tile_horizontal = int(x_horizontal), int(y_horizontal)
 
                 if tile_horizontal in self.game.map.world_map:
+                    texture_horizontal = self.game.map.world_map[tile_horizontal]
                     break
 
                 x_horizontal += dx
@@ -46,6 +75,7 @@ class Raycasting:
                 tile_vertical = int(x_vertical), int(y_vertical)
 
                 if tile_vertical in self.game.map.world_map:
+                    texture_vertical = self.game.map.world_map[tile_vertical]
                     break
 
                 x_vertical += dx
@@ -53,14 +83,25 @@ class Raycasting:
                 depth_verticle += delta_depth
 
             if depth_verticle < depth_horizontal:
-                depth = depth_verticle
+                depth, texture = depth_verticle, texture_vertical
+                y_vertical %= 1
+                offset = y_vertical if cos_a > 0 else (1 - y_vertical)
             else:
-                depth = depth_horizontal
+                depth, texture = depth_horizontal, texture_horizontal
+                x_horizontal %= 1
+                offset = (1 - x_horizontal) if sin_a > 0 else x_horizontal
 
-            pg.draw.line(self.game.screen, "yellow", (100 * ox, 100 * oy),
-                         (100 * ox + 100 * depth * cos_a, 100 * oy + 100 * depth * sin_a), 2)
+            depth *= math.cos(self.game.player.angle - ray_angle)
+            projection_height = SCREEN_DISTANCE / (depth + 0.0001)
+
+            self.ray_casting_result.append(
+                (depth,
+                 projection_height,
+                 texture,
+                 offset))
 
             ray_angle += DELTA_ANGLE
 
     def update(self):
         self.ray_cast()
+        self.get_objects_to_render()
