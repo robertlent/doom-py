52b757d4b853630a99e1b7a44de7b09e8f5115b8
Robert Lent <robert@therobertlent.com>
2023-03-28 23:58:15 -0700

    Add background music during gameplay

--- sound.py
+++ sound.py
@@ -11,11 +11,11 @@ class Sound:
         self.npc_death = pg.mixer.Sound(self.path + 'npc_death.wav')
         self.npc_attack = pg.mixer.Sound(self.path + 'npc_attack.wav')
         self.player_pain = pg.mixer.Sound(self.path + 'player_pain.wav')
-        self.theme = pg.mixer.Sound(self.path + 'theme.mp3')
+        self.theme = pg.mixer.music.load(self.path + 'theme.mp3')
 
         self.shotgun.set_volume(0.3)
         self.npc_pain.set_volume(0.2)
         self.npc_death.set_volume(0.2)
         self.npc_attack.set_volume(0.2)
         self.player_pain.set_volume(0.2)
-        self.theme.set_volume(0.3)
+        pg.mixer.music.set_volume(0.3)
